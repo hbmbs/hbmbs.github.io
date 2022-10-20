f6e143b802e845dc911c8c61e944d4429c87d241
morty101 <belmoudden.hicham@gmail.com>
2022-10-20 04:19:01 +0100

    Update plots.js

--- plots.js
+++ plots.js
@@ -91,5 +91,35 @@ function buildCharts(sample) {
     };
     // 10. Use Plotly to plot the data with the layout. 
     Plotly.newPlot("bar", barData, barLayout);
+
+    // -----------------------------------
+    // Bar and Bubble charts
+    // -----------------------------------
+
+    // 1. Create the trace for the bubble chart.
+    var bubbleData = [
+      {
+        x: ids,
+        y: values,
+        text: labels,
+        mode: "markers",
+        marker: {
+          color: ids,
+          size: values,
+          }
+      }
+    ];
+
+    // 2. Create the layout for the bubble chart.
+    var bubbleLayout = {
+      margin: { t: 0 },
+      xaxis: { title: "OTU ID" },
+      hovermode: "closest"
+      
+    };
+
+    // 3. Use Plotly to plot the data with the layout.
+    Plotly.newPlot("bubble", DataBubble, LayoutBubble); 
   });
 }
+
